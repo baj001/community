a92984a7a19fd38a8e4a54f2a0df32adc561cbd2
baj <fkshang@126.com>
2022-04-04 21:51:09 +0800

    关注和取关

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -27,7 +27,6 @@ public interface CommunityConstant {
      */
     int REMEMBER_EXPIRED_SECONDS = 3600 * 24 * 100;
 
-
     /**
      * 实体类型: 帖子
      */
@@ -37,5 +36,10 @@ public interface CommunityConstant {
      * 实体类型: 评论
      */
     int ENTITY_TYPE_COMMENT = 2;
+
+    /**
+     * 实体类型: 用户
+     */
+    int ENTITY_TYPE_USER = 3;
 
 }
