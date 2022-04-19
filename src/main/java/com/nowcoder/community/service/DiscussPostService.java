748889e8d83d10569a7fa1a53194cee57d77e859
baj <fkshang@126.com>
2022-04-19 15:12:11 +0800

    权限控制  spring版本变更为了2.1.5 ---4.19

--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -18,11 +18,10 @@ public class DiscussPostService {
     @Autowired
     private SensitiveFilter sensitiveFilter;
 
-    //查看 某个用户 的留言 (从多少页开始,每页显示多少)
     public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit) {
         return discussPostMapper.selectDiscussPosts(userId, offset, limit);
     }
-    //显示这个用户的留言一共有多少行
+
     public int findDiscussPostRows(int userId) {
         return discussPostMapper.selectDiscussPostRows(userId);
     }
@@ -46,8 +45,16 @@ public class DiscussPostService {
         return discussPostMapper.selectDiscussPostById(id);
     }
 
-
     public int updateCommentCount(int id, int commentCount) {
         return discussPostMapper.updateCommentCount(id, commentCount);
     }
+
+    public int updateType(int id, int type) {
+        return discussPostMapper.updateType(id, type);
+    }
+
+    public int updateStatus(int id, int status) {
+        return discussPostMapper.updateStatus(id, status);
+    }
+
 }
